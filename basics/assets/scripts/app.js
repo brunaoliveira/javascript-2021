f030992310a902b22c2acfdb80b525d3b686f430
brunaoliveira <brunaol678@gmail.com>
2021-03-24 18:51:59 -0300

    add functionality for all buttons

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -9,9 +9,37 @@ function add() {
     const calculationDescription = `${currentResult} + ${enteredNumber}`;
     currentResult = currentResult + enteredNumber;
     // +userInput.value transform to number, keeping the floating point if appears
+
+    outputResult(currentResult, calculationDescription);
+}
+
+function subtract() {
+    const enteredNumber = getUserInput();
+    const calculationDescription = `${currentResult} - ${enteredNumber}`;
+    currentResult = currentResult - enteredNumber;
+
+    outputResult(currentResult, calculationDescription);
+}
+
+function multiply() {
+    const enteredNumber = getUserInput();
+    const calculationDescription = `${currentResult} * ${enteredNumber}`;
+    currentResult = currentResult * enteredNumber;
+
+    outputResult(currentResult, calculationDescription);
+}
+
+function divide() {
+    const enteredNumber = getUserInput();
+    const calculationDescription = `${currentResult} / ${enteredNumber}`;
+    currentResult = currentResult / enteredNumber;
+
     outputResult(currentResult, calculationDescription);
 }
 
 // não passa com parenteses, só o nome da função, 
 // para que seja executada apenas no evento 'click' e não no momento de compiling do arquivo
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
