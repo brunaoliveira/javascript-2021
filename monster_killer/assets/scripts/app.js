ddfe0ca9bfd016d106aef625108657f87886948b
brunaoliveira <brunaol678@gmail.com>
2021-04-19 15:38:14 -0300

    treat win/lost/draw

--- monster_killer/assets/scripts/app.js
+++ monster_killer/assets/scripts/app.js
@@ -1,4 +1,6 @@
 const MAX_ATTACK_VALUE = 10;
+const MAX_MONSTER_ATTACK_VALUE = 14;
+
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
@@ -6,8 +8,19 @@ let currentPlayerHealth = chosenMaxLife;
 adjustHealthBars(chosenMaxLife);
 
 function attackHandler() {
-    const damage = dealMonsterDamage(MAX_ATTACK_VALUE);
-    currentMonsterHealth -= damage;
+    const monsterDamage = dealMonsterDamage(MAX_ATTACK_VALUE);
+    const playerDamage = dealPlayerDamage(MAX_MONSTER_ATTACK_VALUE);
+
+    currentMonsterHealth -= monsterDamage;
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0){
+        alert('It\'s a draw!');
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
